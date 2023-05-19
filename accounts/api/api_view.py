b47131a26710ac917697de611efe41e539465ff0
Yash <yashrasniya3@gmail.com>
2023-05-19 18:52:45 +0530

    login and sign up is done

--- accounts/api/api_view.py
+++ accounts/api/api_view.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from ..models import User
-from ..serializers import UserSerializer,UserRegisterSerializer
+from ..serializers import UserSerializer, UserRegisterSerializer
 import pyotp
 from ..utils import get_token
 from django.dispatch import Signal
@@ -14,21 +14,20 @@ import smtplib, ssl
 import random
 
 
-def error(message,**kwargs):
+def error(message, **kwargs):
     error_message = {'message': message, 'status': False}
     if kwargs:
         error_message.update(**kwargs)
     return error_message
 
+
 class login(APIView):
     permission_classes = [AllowAny]
 
-
-
     def get(self, request):
-        email = request.GET.get('email', None)
+        email = request.GET.get('mob_number', None)
         if not email:
-            return Response(error('email not found',hi='hi'))
+            return Response(error('mobile not found', hi='hi'))
         user_obj = User.objects.filter(mob_number=email)
         if not user_obj:
             return Response(error('User not found'))
@@ -46,7 +45,7 @@ class Otp_varify(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request):
-        mobile = request.data.get('email', None)
+        mobile = request.data.get('mob_number', None)
         otp = request.data.get('otp', "")
 
         if otp.__len__() != 6:
@@ -89,22 +88,23 @@ class Otp_varify(APIView):
                             status=status.HTTP_200_OK)
         return Response({'message': 'Invalid OTP'}, status=status.HTTP_400_BAD_REQUEST)
 
+
 class register(APIView):
     permission_classes = [AllowAny]
 
-    def post(self,request):
+    def post(self, request):
         if User.objects.filter(mob_number=request.data.get('mob_number')):
-            if User.objects.filter(mob_number=request.data.get('mob_number'),is_active=False):
-                User.objects.get(mob_number=request.data.get('mob_number'),is_active=False).delete()
+            if User.objects.filter(mob_number=request.data.get('mob_number'), is_active=False):
+                User.objects.get(mob_number=request.data.get('mob_number'), is_active=False).delete()
             else:
                 return Response(error('user with this mob number already exists.'))
-        ser_obj=UserRegisterSerializer(data=request.data)
+        ser_obj = UserRegisterSerializer(data=request.data)
 
         if ser_obj.is_valid():
             ser_obj.save()
-            print(dir(ser_obj),ser_obj.data)
-            user_obj=User.objects.get(mob_number=ser_obj.data['mob_number'])
-            user_obj.is_active=False
+            print(dir(ser_obj), ser_obj.data)
+            user_obj = User.objects.get(mob_number=ser_obj.data['mob_number'])
+            user_obj.is_active = False
             user_obj.save()
             time_otp = pyotp.TOTP(user_obj.key)
             time_otp = time_otp.now()
@@ -112,17 +112,16 @@ class register(APIView):
                 {'status': '200', 'message': 'Please verify your mobile to complete signup.', 'otp': time_otp},
                 status.HTTP_200_OK)
         else:
-            return Response(error('some thing is messing',error=ser_obj.errors))
+            return Response(error('some thing is messing', error=ser_obj.errors))
 
 
 class UserProfile(APIView):
     permission_classes = [IsAuthenticated]
 
-    def post(self,request):
-
-        ser=UserSerializer.update(UserSerializer(),request.user,validated_data=request.data)
+    def post(self, request):
+        ser = UserSerializer.update(UserSerializer(), request.user, validated_data=request.data)
 
         return Response(UserSerializer(request.user).data)
 
-    def get(self,request):
+    def get(self, request):
         return Response(UserSerializer(request.user).data)
