d924f532ebb41e152f22369a5b551bca2fbca08c
Yash <yashrasniya3@gmail.com>
2023-06-23 22:20:51 +0530

    booking part done

--- booking/serializers.py
+++ booking/serializers.py
@@ -28,5 +28,5 @@ class Free_trial_serializers(serializers.ModelSerializer):
 
 class Free_trial_serializers_user(Free_trial_serializers):
     def __init__(self,instance=None, data=empty, **kwargs):
-        super(my_gym, self).__init__(instance=instance, data=data, **kwargs)
+        super(Free_trial_serializers, self).__init__(instance=instance, data=data, **kwargs)
         self.Meta.fields.append('token')
