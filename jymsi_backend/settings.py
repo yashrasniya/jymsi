3ccea411dc1986f597c01f6c72fb78fdf17af064
Yash <yashrasniya3@gmail.com>
2023-07-21 20:02:06 +0530

    ./au

--- jymsi_backend/settings.py
+++ jymsi_backend/settings.py
@@ -27,7 +27,7 @@ DEBUG = False
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True  # any web side have access to my api
 CORS_URLS_REGEX = r'^/api/.*$'
-CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5500','http://localhost:3000','http://www.zymsi.com','https://zymsi.netlify.app']
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5500','http://localhost:3000','http://www.zymsi.com','https://www.zymsi.com','https://zymsi.netlify.app']
 
 ALLOWED_HOSTS = ['184.72.212.75','www.zymsi.com/','zymsi.netlify.app','*']
 
