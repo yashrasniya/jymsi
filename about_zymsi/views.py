4259bac4664e5741c6a40eef3424b3d0d9d0ff77
Yash <yashrasniya3@gmail.com>
2023-08-01 09:29:33 +0530

    dsfafa

--- about_zymsi/views.py
+++ about_zymsi/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
 from .models import About
-from django.http import HttpResponse
-from django.http import JsonResponse
-import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.forms.models import model_to_dict
-# Create your views here.
+from .serializers import About_serializers
+
 
 class about_view(APIView):
     def get(self,request):
         print(About.objects.first())
-        return Response(About.objects.all().values())
+        return Response(About_serializers(About.objects.all(),many=True,context={'request':request,'user':request.user}).data)
